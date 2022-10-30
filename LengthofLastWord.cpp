ead3d94aac404fb26fbe388157fadf88bfb3328b
Sarthak Srivastava <114425208+Sarthak-107@users.noreply.github.com>
2022-10-30 22:59:28 +0530

    Add files via upload

---
+++ LengthofLastWord.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        for(int i = (s.length()-1);i>=0;i--)
+        {
+            if((s[i]==' ')&&(count==0))
+                continue;
+            else if(s[i]==' ')
+                return count;
+            else
+                count++;    
+        }
+        return count;
+    }
+};
