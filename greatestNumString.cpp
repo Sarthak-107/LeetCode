b4ec9470d05d73326671a1d07b42093d0e133c56
Sarthak Srivastava <114425208+Sarthak-107@users.noreply.github.com>
2022-11-02 23:23:56 +0530

    Add files via upload

---
+++ greatestNumString.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+    string s;
+    cout<<"Enter the string : ";
+    getline(cin,s);
+    sort(s.begin(),s.end(),greater<int>());
+    cout<<s;
+}
