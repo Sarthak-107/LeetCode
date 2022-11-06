b9b078abd62254674b75f0288684c642c425a800
Sarthak Srivastava <114425208+Sarthak-107@users.noreply.github.com>
2022-11-06 22:11:10 +0530

    Add files via upload

---
+++ spiralOrderMatrixTraversal.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N,M;
+    cout<<"Enter the number of rows and columns :"<<endl;
+    cin>>N>>M;
+    int arr[N][M];
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int count=0,i=0,j=0;
+    int total=N*M;
+    while(count=N*M)
+    {
+      count++;
+      if(i==j)
+      {
+        cout<<arr[]
+      }
+    }
+}
